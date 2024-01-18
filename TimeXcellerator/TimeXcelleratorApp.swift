322601e4ea9490c3ce9ef94c5d39b1468ec6af55
Ionut Ciovica <ionut.ciovica@gmail.com>
2024-01-18 12:15:53 +0200

    Initial Commit

---
+++ TimeXcellerator/TimeXcelleratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeXcelleratorApp.swift
+//  TimeXcellerator
+//
+//  Created by IonutCiovica on 18/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimeXcelleratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
